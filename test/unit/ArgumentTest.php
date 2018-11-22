e5f87cb03ad1a8a3cea6ace6fd6fe3ffd32433f8
Greg Bowler <greg.bowler@g105b.com>
2018-11-22 16:08:29 +0000

    Test toString on long argument

--- test/unit/ArgumentTest.php
+++ test/unit/ArgumentTest.php
@@ -1,16 +1,25 @@
 <?php
 namespace Gt\Cli\Test;
 
+use Gt\Cli\Argument\LongOptionArgument;
 use Gt\Cli\Argument\NamedArgument;
 use Gt\Cli\Argument\ShortOptionArgument;
 use PHPUnit\Framework\TestCase;
 
 class ArgumentTest extends TestCase {
-	public function testToString() {
+	public function testToStringNamed() {
 		$value = uniqid();
 		$arg = new NamedArgument($value);
 
 		self::assertEquals($value, (string)$arg);
+	}
+
+	public function testToStringLongOption() {
+		$name = uniqid();
+		$value = uniqid();
+		$arg = new LongOptionArgument("--$name", $value);
+
+		self::assertEquals("$name:$value", (string)$arg);
 	}
 
 	public function testShortOption() {
