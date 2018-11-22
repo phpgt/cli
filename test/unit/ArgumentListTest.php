99a1e62a50736d91eb3a639d98cd5eba0538202d
Greg Bowler <greg.bowler@g105b.com>
2018-11-22 18:49:08 +0000

    Test getIterator with named args

--- test/unit/ArgumentListTest.php
+++ test/unit/ArgumentListTest.php
@@ -1,11 +1,12 @@
 <?php
 namespace Gt\Cli\Test;
 
+use Gt\Cli\Argument\Argument;
 use Gt\Cli\Argument\ArgumentList;
 use PHPUnit\Framework\TestCase;
 
 class ArgumentListTest extends TestCase {
-	/** @dataProvider data_randomArgs */
+	/** @dataProvider data_randomNamedArgs */
 	public function testGetCommandName(string...$args) {
 		$argumentList = new ArgumentList(
 			array_shift($args),
@@ -17,7 +18,28 @@ class ArgumentListTest extends TestCase {
 		);
 	}
 
-	public function data_randomArgs():array {
+	/** @dataProvider data_randomNamedArgs */
+	public function testIteratorWithNamedArgs(string...$args) {
+		$argumentList = new ArgumentList(
+			array_shift($args),
+			...$args
+		);
+
+		foreach($argumentList as $i => $argument) {
+			/** @var Argument $argument */
+			self::assertInstanceOf(
+				Argument::class,
+				$argument
+			);
+
+			self::assertEquals(
+				$args[$i],
+				$argument
+			);
+		}
+	}
+
+	public function data_randomNamedArgs():array {
 		$dataSet = [];
 
 		for($i = 0; $i < 10; $i++) {
@@ -29,6 +51,31 @@ class ArgumentListTest extends TestCase {
 			$numParams = rand(1, 10);
 			for($j = 0; $j < $numParams; $j++) {
 				$params []= uniqid();
+			}
+
+			$dataSet []= $params;
+		}
+
+		return $dataSet;
+	}
+
+	public function data_randomLongArgs():array {
+		$dataSet = [];
+
+		for($i = 0; $i < 10; $i++) {
+			$params = [];
+
+			$params []= uniqid("script-");
+			$params []= uniqid("command-");
+
+			$numParams = rand(1, 10);
+			for($j = 0; $j < $numParams; $j++) {
+				if($j % 2 === 0) {
+					$params []= "--" . uniqid();
+				}
+				else {
+					$params []= uniqid();
+				}
 			}
 
 			$dataSet []= $params;
