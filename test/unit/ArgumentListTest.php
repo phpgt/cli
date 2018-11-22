2aa9081fa081d72b7616851dbfb236870d3fba27
Greg Bowler <greg.bowler@g105b.com>
2018-11-22 21:42:19 +0000

    Break out contains tests

--- test/unit/ArgumentListTest.php
+++ test/unit/ArgumentListTest.php
@@ -3,6 +3,7 @@ namespace Gt\Cli\Test;
 
 use Gt\Cli\Argument\Argument;
 use Gt\Cli\Argument\ArgumentList;
+use Gt\Cli\Parameter\Parameter;
 use PHPUnit\Framework\TestCase;
 
 class ArgumentListTest extends TestCase {
@@ -75,6 +76,120 @@ class ArgumentListTest extends TestCase {
 		}
 	}
 
+	/** @dataProvider data_randomShortArgs */
+	public function testIteratorWithShortArgs(string...$args) {
+		$argumentList = new ArgumentList(
+			array_shift($args),
+			...$args
+		);
+
+		foreach($argumentList as $i => $argument) {
+			/** @var Argument $argument */
+			self::assertInstanceOf(
+				Argument::class,
+				$argument
+			);
+
+			if($i === 0) {
+				self::assertEquals(
+					$args[0],
+					$argument
+				);
+				continue;
+			}
+
+			$originalKey = $args[($i - 1) * 2 + 1];
+			$originalValue = $args[($i - 1) * 2 + 2];
+
+			self::assertEquals(
+				substr($originalKey, 1),
+				$argument->getKey()
+			);
+			self::assertEquals(
+				$originalValue,
+				$argument->getValue()
+			);
+		}
+	}
+
+	/** @dataProvider data_randomShortArgs */
+	public function testContainsWithShortArgs(string...$args) {
+		$argumentList = new ArgumentList(
+			array_shift($args),
+			...$args
+		);
+
+		foreach($args as $i => $arg) {
+			if($i === 0) {
+				continue;
+			}
+
+			if($i % 2 === 0) {
+// Because args are passed in as "--key value" every other original arg is skipped.
+				continue;
+			}
+
+			$param = self::createMock(Parameter::class);
+			$param->method("getShortOption")
+				->willReturn(substr($arg, 1));
+
+			/** @var Parameter $param */
+			self::assertTrue($argumentList->contains($param));
+		}
+	}
+
+	/** @dataProvider data_randomLongArgs */
+	public function testContainsWithLongArgs(string...$args) {
+		$argumentList = new ArgumentList(
+			array_shift($args),
+			...$args
+		);
+
+		foreach($args as $i => $arg) {
+			if($i === 0) {
+				continue;
+			}
+
+			if($i % 2 === 0) {
+// Because args are passed in as "--key value" every other original arg is skipped.
+				continue;
+			}
+
+			$param = self::createMock(Parameter::class);
+			$param->method("getLongOption")
+				->willReturn(substr($arg, 2));
+
+			/** @var Parameter $param */
+			self::assertTrue($argumentList->contains($param));
+		}
+	}
+
+	/** @dataProvider data_randomLongArgs */
+	public function testNotContains(string...$args) {
+		$argumentList = new ArgumentList(
+			array_shift($args),
+			...$args
+		);
+
+		foreach($args as $i => $arg) {
+			if($i === 0) {
+				continue;
+			}
+
+			if($i % 2 === 0) {
+// Because args are passed in as "--key value" every other original arg is skipped.
+				continue;
+			}
+
+			$param = self::createMock(Parameter::class);
+			$param->method("getLongOption")
+				->willReturn(uniqid());
+
+			/** @var Parameter $param */
+			self::assertFalse($argumentList->contains($param));
+		}
+	}
+
 	public function data_randomNamedArgs():array {
 		$dataSet = [];
 
@@ -108,6 +223,31 @@ class ArgumentListTest extends TestCase {
 			for($j = 0; $j < $numParams; $j++) {
 				if($j % 2 === 0) {
 					$params []= "--" . uniqid();
+				}
+				else {
+					$params []= uniqid();
+				}
+			}
+
+			$dataSet []= $params;
+		}
+
+		return $dataSet;
+	}
+
+	public function data_randomShortArgs():array {
+		$dataSet = [];
+
+		for($i = 0; $i < 10; $i++) {
+			$params = [];
+
+			$params []= uniqid("script-");
+			$params []= uniqid("command-");
+
+			$numParams = rand(1, 10);
+			for($j = 0; $j < $numParams; $j++) {
+				if($j % 2 === 0) {
+					$params []= "-" . uniqid();
 				}
 				else {
 					$params []= uniqid();
