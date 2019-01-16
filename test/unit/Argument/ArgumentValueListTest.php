f1617ad79a36488034491f93e6fdd8ce85f33566
Greg Bowler <greg.bowler@g105b.com>
2019-01-16 14:56:59 +0000

    Test get/set argument value list

--- test/unit/Argument/ArgumentValueListTest.php
+++ test/unit/Argument/ArgumentValueListTest.php
@@ -7,9 +7,18 @@ use Gt\Cli\Argument\ArgumentValueListNotSetException;
 use PHPUnit\Framework\TestCase;
 
 class ArgumentValueListTest extends TestCase {
-	public function testGetSetNotSet() {
+	public function testGetNotSet() {
 		$avl = new ArgumentValueList();
 		$this->expectException(ArgumentValueListNotSetException::class);
 		$avl->get("test");
+	}
+
+	public function testGetSet() {
+		$avl = new ArgumentValueList();
+		$avl->set("test", "example-value");
+		self::assertEquals(
+			"example-value",
+			$avl->get("test")
+		);
 	}
 }
