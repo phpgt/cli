363ec202fac3ed95b9cf85cc8a51597675acd57e
Greg Bowler <greg.bowler@g105b.com>
2019-02-23 22:25:08 +0000

    Set default argument value correctly

--- src/Argument/DefaultArgumentValue.php
+++ src/Argument/DefaultArgumentValue.php
@@ -3,6 +3,7 @@ namespace Gt\Cli\Argument;
 
 class DefaultArgumentValue extends ArgumentValue {
 	public function __construct(string $default) {
-		parent::__construct($default);
+		parent::__construct("__DEFAULT__");
+		$this->push($default);
 	}
 }
